a7165b175c3a277f68fcaaae777394d6fa995ebd
besen <besen@elcimai.com>
2020-09-22 17:18:20 +0200

    ajout du .SLN pour ouvrir l'exo 2

---
+++ ProgrammationObjet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammationObjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
